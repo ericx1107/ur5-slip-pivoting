190e2f42caeef4419164d51f8829584ee99e62c7
ericx1107 <xushiyu1107@gmail.com>
2023-01-31 11:43:14 +1100

    Update synchroniser to plot

--- scripts/synchronise_data.py
+++ scripts/synchronise_data.py
@@ -4,6 +4,7 @@ import rospy
 import message_filters
 import csv
 import numpy as np
+import matplotlib.pyplot as plt
 from slip_manipulation.msg import AngleStamped
 from geometry_msgs.msg import WrenchStamped
 
@@ -11,36 +12,48 @@ class SyncData:
   def __init__(self):
     self.fz_array = []
     self.angle_array = []
+    self.data = []
     self.inc = 0
 
-    wrench_sub = message_filters.Subscriber('robotiq_ft_wrench', WrenchStamped)
-    angle_sub = message_filters.Subscriber('/slip_manipulation/rotation_angle', AngleStamped)
+    self.wrench_sub = message_filters.Subscriber('/robotiq_ft_wrench', WrenchStamped)
+    self.angle_sub = message_filters.Subscriber('/slip_manipulation/rotation_angle', AngleStamped)
 
-    ts = message_filters.TimeSynchronizer([wrench_sub, angle_sub], 10)
-    ts.registerCallback(self.callback)
+    self.ts = message_filters.ApproximateTimeSynchronizer([self.wrench_sub, self.angle_sub], 10, slop=0.00005)
+    self.ts.registerCallback(self.callback)
 
   def callback(self, wrench_stamped, angle_stamped):
     self.inc = 0  # reset increment
 
     fz = wrench_stamped.wrench.force.z
     angle = angle_stamped.angle
-
+    self.data.append([fz, angle])
     self.fz_array.append(fz)
     self.angle_array.append(angle)
+    print('callback')
 
 if __name__ == "__main__":
     rospy.init_node("process_synced_data")
     
     proc = SyncData()
 
+    # rospy.spin()
     while not rospy.is_shutdown():
-      angle = rospy.wait_for_message('/slip_manipulation/rotation_angle', AngleStamped, timeout=rospy.Duration(5))
-
-      if angle is None:
+      try:
+        angle = rospy.wait_for_message('/slip_manipulation/rotation_angle', AngleStamped, timeout=rospy.Duration(5))
+        # print('looping')
+      except rospy.exceptions.ROSException:
         break
+
+    fig, ax = plt.subplots()
+    
+    ax.scatter(proc.angle_array, proc.fz_array)
+    ax.set_xlabel('Rotation (degrees)')
+    ax.set_ylabel('Force in the z direction (N)')
+    
+    fig.savefig('/home/acrv/eric_ws/bagfiles/data.png')
 
     with open("/home/acrv/eric_ws/bagfiles/data.csv", "wb") as f:
       writer = csv.writer(f)
-      writer.writerows(np.concatenate(proc.angle_array, proc.fz_array))
+      writer.writerows(proc.data)
 
     
