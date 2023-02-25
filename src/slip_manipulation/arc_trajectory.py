Reorganise scripts for open/closed loop, add pick and place method, fix trajectory and gripper controllers, add work calculation, add vision controller

src/slip_manipulation/arc_trajectory.py
@@ -6,14 +6,17 @@ import math
 import numpy as np
 import tf2_ros
 import time
+import moveit_commander
 from geometry_msgs.msg import Pose, Point, Quaternion
 from slip_manipulation.get_tf_helper import *
-from std_msgs.msg import Bool
+from std_msgs.msg import Bool,Float32
 from geometry_msgs.msg import WrenchStamped
 from slip_manipulation.msg import AngleStamped
 from papillarray_ros_v2.msg import SensorState
 from slip_manipulation.sensorised_gripper import SensorisedGripper
-import time
+from control_msgs.msg import FollowJointTrajectoryActionGoal
+from actionlib_msgs.msg import GoalStatusArray
+from moveit_msgs.msg import RobotState
 
 class ArcTrajectory():
     # pose: Current Pose of the end effector
@@ -26,7 +29,7 @@ class ArcTrajectory():
     '''
     Always pivots towards the centre of box
     '''
-    def __init__(self, box_dim, arm, grasp_param, box_weight):
+    def __init__(self, box_dim, ur5_moveit, gripper, grasp_param, box_weight):
         self.long = rospy.wait_for_message('/slip_manipulation/is_long_edge', Bool, timeout=rospy.Duration(10))
         
         if self.long.data:
@@ -42,18 +45,29 @@ class ArcTrajectory():
             self.height_dim = box_dim[0]
             self.grasp_param = grasp_param
         
+        # challenge offset
+        self.base_dim += 0.05
+        
         self.listener = tf.TransformListener()
         self.tf_buffer = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
 
         self.angle_sub = rospy.Subscriber('/slip_manipulation/rotation_angle', AngleStamped, self.angle_callback)
         self.ft_sub = rospy.Subscriber('/robotiq_ft_wrench', WrenchStamped, self.ft_callback)
-        self.tac_sub = rospy.Subscriber('/hub_0/sensor_0', SensorState, self.tac_callback)
+        # self.tac_sub = rospy.Subscriber('/hub_0/sensor_0', SensorState, self.tac_callback)
         # self.gripper_sub = rospy.Subscriber('/Robotiq2FGripperRobotInput', inputMsg.Robotiq2FGripper_robot_input, self.gripper_callback)
-        self.gripper = SensorisedGripper()
+        self.gripper = gripper
         # self.gripper_pub = rospy.Publisher('/Robotiq2FGripperRobotOutput', outputMsg.Robotiq2FGripper_robot_output, queue_size=1)
         
-        self.arm = arm
+        self.goal_pub = rospy.Publisher('/scaled_pos_joint_traj_controller/follow_joint_trajectory/goal', FollowJointTrajectoryActionGoal, queue_size=1)
+        
+        self.work_pub = rospy.Publisher('/slip_manipulation/work', Float32, queue_size=1)
+        
+        
+        
+        self.ur5 = ur5_moveit
+        self.arm = ur5_moveit.arm
+        self.robot = moveit_commander.RobotCommander()
 
         self.theta = 0
         self.tac_data = 0
@@ -63,23 +77,47 @@ class ArcTrajectory():
 
         self.angle_err = 1e6 # set large number to initialise while loop
         self.threshold = 5e-1
+        # PID constants
         self.Kp = 2e-4#1e-4
         self.Kd = 5e-4
         self.Ki = 8e-5#3e-6
+        self.Kp_vision = 2e-4#1e-4
+        self.Ki_vision = 8e-5#3e-6
         
         self.z_offset = 0
         self.x_offset = 0
         
         self.temp_loop = np.arange(5)
         
-        self.theta = 0
-        self.dt = time.time()
+        self.dt = 0
         self.prev_err = 0
         self.intg_err = 0
         
         self.slip_type = 0
+        self.slip_type_x = 0
         self.pillars_in_contact = 0
+        self.safety_bound = 40
         
+        self.prev_dt = time.time()
+        
+        self.original = None
+        
+        self.prev_angle = 0
+        
+        # self.start_z = 0
+        # self.start_y = 0
+        # self.start_theta = 0
+        
+        # self.end_z = 0
+        # self.end_y = 0
+        # self.end_theta = 0
+        
+        self.translational_work = 0
+        self.rotational_work = 0
+        self.grasped = False
+        
+        
+        self.init_grip_width = None
 
     def plan_cartesian_path(self, curr_angle, goal_angle):
         # Always assuming we start from the long direction
@@ -94,8 +132,9 @@ class ArcTrajectory():
             # change is always positive
             # cos is always positive -> change * cos is positive -> x is positive
             # sin is always negative -> change * sin is negative -> y is negative
-        for theta in np.linspace(box_angle, box_angle + goal_angle, 2 * int(np.ceil(0.5*abs(goal_angle - curr_angle)))):
-            wpose.position.z = -(r*math.sin(math.radians(theta))) + self.height_dim# - 0.03
+            # int(np.ceil(abs(goal_angle - curr_angle)))
+        for theta in np.linspace(box_angle, box_angle + goal_angle, 50):
+            wpose.position.z = -(r*math.sin(math.radians(theta))) + self.height_dim # - 0.03
             wpose.position.y = self.base_dim - r*math.cos(math.radians(theta))
             
             wpose_base = tf_transform_pose(self.listener, wpose, 'tool0', 'base_link').pose
@@ -108,9 +147,34 @@ class ArcTrajectory():
         
     def ft_callback(self, data):
         self.Fz = data.wrench.force.z
+        self.Fy = data.wrench.force.y
+        self.Tx = data.wrench.torque.x
+        
+        trans = patient_lookup_tf(self.tf_buffer, 'tool0')
+        
+        if self.grasped:
+            # print("Fz is ", self.Fz)
+            # print("Distance change is ", abs(trans.transform.translation.z - self.original.pose.position.z))
+            self.translational_work  += abs(self.Fz * abs(trans.transform.translation.z - self.original.pose.position.z))
+            self.translational_work  += abs(self.Fy * abs(trans.transform.translation.y - self.original.pose.position.y))
+            
+            self.original.pose.position.z = trans.transform.translation.z 
+            self.original.pose.position.y = trans.transform.translation.y
+        
+        
+            angle_diff = abs(self.theta - self.prev_angle) 
+            self.prev_angle = self.theta
+            
+            self.rotational_work += abs((angle_diff) * self.Tx)
+        
+        
+        
         
     def tac_callback(self, data):
         self.tac_data = data
+        
+        if self.init_grip_width is not None:
+            self.slip_control(self.init_grip_width)
         
     def force_pred(self):
         # force = 20/np.pi * np.arctan(-8 * (self.theta - np.pi/5)) - 6
@@ -119,10 +183,18 @@ class ArcTrajectory():
         force = self.fg * np.sin(theta) * np.cos(alpha)/2
         return force
 
-    def control_robot(self, goal_angle):
+    def control_robot(self, goal_angle, init_grip_width=None):
         waypoints = self.plan_cartesian_path(self.theta, goal_angle)
-        self.time = time.time()
-        while self.angle_err > 1 or waypoints == []:
+        inc = 0     # also for faster goal publishing
+        
+        self.init_grip_width = init_grip_width
+        
+        while self.angle_err > 1 and waypoints != []:
+            # self.start_theta = self.theta
+            # self.start_z = self.arm.get_current_pose().pose.position.z
+            # self.start_y = self.arm.get_current_pose().pose.position.y
+            
+            
             temp_waypoints = []
             
             
@@ -134,17 +206,68 @@ class ArcTrajectory():
             temp_waypoints.append(copy.deepcopy(waypoints[0]))
             waypoints.pop(0)
             # print(temp_waypoints)
-            (plan, _) = self.arm.compute_cartesian_path(temp_waypoints, 0.01, 0.0)
-            execute_time = time.time()
-            self.arm.execute(plan, wait=True)
-            execute_time = time.time() - execute_time
-            self.slip_control()
-            # if self.z_offset < 0.05:
-            #     self.arm.execute(plan, wait=True)
+            # rospy.sleep(0.3)
+            self.dt = time.time()
+            
+            
+            ################################### faster goal publishing ################################################
+            # if inc == 0:
+            #     # run normal plan for the first movement
+            #     (plan, _) = self.arm.compute_cartesian_path(temp_waypoints, 0.01, 0.0)
+                
+            #     # set up goal message
+            #     goal_msg = FollowJointTrajectoryActionGoal()
+            #     goal_msg.goal_id.stamp = rospy.Time.now()
+            #     goal_msg.goal.trajectory = plan.joint_trajectory
+                
+            #     # self.dt = time.time()
             # else:
-            #     rospy.logerr("nah")
+            #     # use previous goal as start state
+            #     start_state = RobotState()
+            #     start_state.joint_state.header.frame_id = 'base_link'
+            #     start_state.joint_state.header.stamp = rospy.Time.now()
+            #     start_state.joint_state.name = plan.joint_trajectory.joint_names
+            #     start_state.joint_state.position = plan.joint_trajectory.points[-1].positions
+            #     start_state.joint_state.velocity = plan.joint_trajectory.points[-1].velocities
+            #     start_state.joint_state.effort = plan.joint_trajectory.points[-1].effort
+            #     start_state.is_diff = False
+            #     (plan, _) = self.ur5.compute_cartesian_path_with_start_state(start_state, temp_waypoints, 0.01, 0.0)
+                
+            #     # set up goal
+            #     goal_msg = FollowJointTrajectoryActionGoal()
+            #     goal_msg.goal_id.stamp = rospy.Time.now()
+            #     goal_msg.goal.trajectory = plan.joint_trajectory
+
+            #     # self.dt = time.time()    
+            #     # rospy.sleep(0.2)
+            #     # wait for previous goal to complete
+            #     # t = time.tsquare_short_open_without_2023-02-22-13-18-22 = rospy.wait_for_message('/scaled_pos_joint_traj_controller/follow_joint_trajectory/status', GoalStatusArray)
+            #         except:
+            #             pass
+            #         # print('got status message')
+            #         # print('goal_id',resp.status_list[-1].goal_id)
+            #         # print('status',resp.status_list[-1].status)
+                    
+            #         if resp.status_list[-1].status == 3:
+            #             # print('previous goal successful\n\n\n\n')
+            #             # print(resp.status_list[-1].status)
+            #             # print(time.time() - t)
+            #             break
+            
+            # inc += 1
+
+            
+            # self.goal_pub.publish(goal_msg)
+            ##########################################################################################################
+            
+            (plan, _) = self.arm.compute_cartesian_path(temp_waypoints, 0.01, 0.0)
+            
+            self.arm.execute(plan, wait=True)
             # execute_time = time.time() - execute_time
             # rospy.sleep(1.5)
+            self.dt = time.time() - self.dt
+            
+            # get error between ideal and measured force
             predicted_force = self.force_pred()
             measured_force = self.Fz
 
@@ -153,14 +276,15 @@ class ArcTrajectory():
             # print("pred:", predicted_force)
             # print("meas:", measured_force)
             # print("error:", err)
-            self.dt = execute_time
             # print('dt: ', self.dt)
-            deriv_err = (err - self.prev_err) / self.dt 
+            # deriv_err = (err - self.prev_err) / self.dt 
+            # accumulate integral error
             self.intg_err += err * self.dt
             # print("error:", err)
             # print("deriv error:", deriv_err)
             # print("intg error:", self.intg_err)
             
+            # PI controller to minimise error
             # Error is positive when the robot is pushing too much, therefore the robot should raise its arm to reduce the force
             if err > self.threshold:
                 # pass
@@ -182,38 +306,127 @@ class ArcTrajectory():
 
             
             self.angle_err = goal_angle - np.degrees(self.theta)
-            self.time = time.time()
             self.prev_err = err
             
-            # print("Angle error: ", self.angle_err)
             
-        print("Pivot complete!")
+            # self.end_theta = self.theta
+            # self.end_z = self.arm.get_current_pose().pose.position.z
+            # self.end_y = self.arm.get_current_pose().pose.position.y
+            
+            
+            
+            # current_work_z = abs((self.end_z - self.start_z) * self.Fz)
+            # current_work_y = abs((self.end_y - self.start_y) * self.Fy)
+            
+            
+            # self.translational_work += current_work_z
+            # self.translational_work += current_work_y
+            # self.rotational_work += abs((self.start_theta - self.end_theta) * self.Tx)
+            
+            # print("Angle error: ", self.angle_err)
         
-    def slip_control(self):
+        
+        # hack to stop gripper control
+        self.init_grip_width =  None
+        
+        print("Pivot complete!")
+        print("Transalational work", self.translational_work)
+        print("Rotational work", self.rotational_work)
+        
+        self.work_pub.publish((self.rotational_work + self.translational_work))
+        print("Total work", (self.rotational_work + self.translational_work))
+        
+    def slip_control(self, init_grip_width):
         stop_tightening = 1
         for pillar in self.tac_data.pillars:
-            if abs(pillar.dX) > 2.5 or abs(pillar.dY) > 2.5 or abs(pillar.dZ) > 2.5:
+            if abs(pillar.dX) > 8 or abs(pillar.dY) > 5 or abs(pillar.dZ) > 5:
                 self.gripper.send_gripper_command(commandName = None, grip_width=self.gripper.grip_width - 1)
                 stop_tightening = 0
                 print("Loosening grip")
             if abs(pillar.dY) > 0.1 and np.sign(pillar.dY) == -1:
                 self.slip_type += 1
+            if abs(pillar.dX) > 0.1 and np.sign(pillar.dX) == -1:
+                self.slip_type_x += 1
             if pillar.in_contact:
                 self.pillars_in_contact += 1
-        print(self.slip_type)
-        print(self.pillars_in_contact)
-        if self.slip_type == self.pillars_in_contact:
-            print('Translational Slip')
-            if self.gripper.grip_width  < 140 and stop_tightening:
-                self.gripper.send_gripper_command(commandName = None, grip_width=self.gripper.grip_width  + 1)
+        # print('slip type', self.slip_type)
+        # print('pillars in contact', self.pillars_in_contact)
+                
+        if self.slip_type == self.pillars_in_contact or self.slip_type_x == self.pillars_in_contact: # Translational Slip
+            if self.gripper.grip_width < init_grip_width + self.safety_bound and stop_tightening:
+                self.gripper.send_gripper_command(commandName = None, grip_width=self.gripper.grip_width + 1)
                 print('Tightening grip')
-            print(self.gripper.grip_width)
+                if self.gripper.grip_width + 1 == init_grip_width + self.safety_bound:
+                    print("Reached safety bound:", self.gripper.grip_width)        
+                else:
+                    print(self.gripper.grip_width)
         
-            
         self.slip_type = 0
         self.pillars_in_contact = 0
         
+    def vision_control_robot(self, goal_angle, init_grip_width=None):
+        '''
+        z offset from height from surface.
+        do not run at the same time as control_robot
+        '''
+        
+        # initialise ideal arc trajectory
+        waypoints = self.plan_cartesian_path(self.theta, goal_angle)
+        
+        # set parameter for gripper control, no control if value is None
+        self.init_grip_width = init_grip_width
+        
+        # loop through ideal trajectory waypoints, stop early if reached goal angle
+        while goal_angle - np.degrees(self.theta) > 1 and waypoints != []:
+            # PI controller to minimise box position error
+            # get error between ideal and measured distance from surface
+            lowest_vertex = rospy.wait_for_message('/slip_manipulation/lowest_vertex', Point)
+            surface_height = 0.0    # hardcoded surface height
+            height_err = lowest_vertex.z - surface_height
             
+            # accumulate integral error, initially dt=0 so no error accumulated
+            self.intg_err += height_err * self.dt
+            
+            # Error is positive when the robot is pushing too much, therefore the robot should raise its arm to reduce the force
+            if height_err > self.threshold:
+                # pass
+                self.z_offset += ((self.Kp_vision * height_err)) + (self.Ki_vision * self.intg_err)
+            # Error is negative when the robot is lifting too much, therefore the robot should lower its arm to increase the force
+            elif height_err < -self.threshold:
+                # pass
+                self.z_offset += (self.Kp * height_err) + (self.Ki * self.intg_err)
+            # Do nothing
+            else: 
+                pass
+            
+            # initialise time difference dt
+            self.dt = time.time()
+            
+            print('z_offset: ', self.z_offset)
+            
+            waypoints[0].position.z = waypoints[0].position.z + self.z_offset
+            waypoints[0].position.x = waypoints[0].position.x + self.x_offset
+
+
+            # execute one waypoint
+            (plan, _) = self.arm.compute_cartesian_path([waypoints[0]], 0.01, 0.0)
+            waypoints.pop(0)
+            
+            self.arm.execute(plan, wait=True)
+
+            # calculate dt for execution
+            self.dt = time.time() - self.dt
+        
+        # hack to stop gripper control once pivot is finished
+        self.init_grip_width = None
+        
+        # print work for recording results
+        print("Pivot complete!")
+        print("Transalational work", self.translational_work)
+        print("Rotational work", self.rotational_work)
+        
+        self.work_pub.publish((self.rotational_work + self.translational_work))
+        print("Total work", (self.rotational_work + self.translational_work))            
 
 if __name__ == "__main__":
     pass

src/slip_manipulation/box_markers.py
@@ -39,9 +39,9 @@ class BoxMarkers():
         self.tf_broadcaster = tf2_ros.TransformBroadcaster()
         
         self.listener = tf.TransformListener()
-        rospy.sleep(0.3)
         self.tf_buffer = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+        rospy.sleep(0.3)
         
         # parameters
         self.box_pose = Pose()
@@ -219,7 +219,7 @@ class BoxMarkers():
         grasp_pose.orientation.z = 0
         grasp_pose.orientation.w = 1
 
-        finger_offset = 0.01 # offset from side of box to grasp position
+        finger_offset = 0.011 # offset from side of box to grasp position
 
         # get rotation matrix for condition check
         orientation_rpy = np.array(tf.transformations.euler_from_quaternion([trans.transform.rotation.x, 

src/slip_manipulation/sensorised_gripper.py
@@ -20,9 +20,9 @@ class SensorisedGripper():
 
         # max gripper width 180
         self.grip_width = 0
-        self.grip_max = 240
+        self.grip_bound = 240   # gripper width that fully closes the grippers when tactile sensors are attached
         self.grip_dist = 0.074 # m
-        self.grip_inc = self.grip_max / self.grip_dist
+        self.grip_inc = self.grip_bound / self.grip_dist
         
         # set up tactile sensor subscribers
         self.tac0_data = SensorState()
@@ -45,6 +45,10 @@ class SensorisedGripper():
 
         # wait for subscribers to initialise and update data in class parameters
         rospy.sleep(1)
+        
+        self.safety_bound = 20
+        self.max_grip_width = 255
+        self.fric_coef = 0.15
 
     def send_gripper_command(self, commandName="deactivate", grip_width=None):
         '''
@@ -52,7 +56,7 @@ class SensorisedGripper():
         shitty implementation of manual setting of grip width, feed None into commandName to use
         '''
         command = outputMsg.Robotiq2FGripper_robot_output()
-        if (grip_width is not None) and (255 >= grip_width >= 0):
+        if (grip_width is not None) and (self.max_grip_width >= grip_width >= 0):
             command.rACT = 1
             command.rGTO = 1
             command.rPR = grip_width
@@ -137,7 +141,6 @@ class SensorisedGripper():
         
         both_contact = 0
         fg = 9.8 * box_weight
-        print('fg', fg)
         long = rospy.wait_for_message('/slip_manipulation/is_long_edge', Bool, timeout=rospy.Duration(10))
         
         if long.data:
@@ -150,12 +153,8 @@ class SensorisedGripper():
             height_dim = box_dim[0]
         init_ft_force = fg/2 * np.sin(np.pi/2 - np.arctan(height_dim/base_dim)) * np.cos(np.arctan(height_dim/base_dim))
         
-        init_grip_width =  255 - int(self.grip_inc * box_dim[2])
+        init_grip_width =  self.max_grip_width - int(self.grip_inc * box_dim[2])
         print('init grip width', init_grip_width)
-        # raw_input("check grip width")
-        # fully open gripper first
-        # self.send_gripper_command(None, 0)
-        # rospy.sleep(0.1)
         self.send_gripper_command(None, init_grip_width)
         rospy.sleep(2)
 
@@ -163,29 +162,22 @@ class SensorisedGripper():
         while not both_contact:
             # increment grip tightness
             print('grip width', self.grip_width)
-            if self.grip_width < init_grip_width + 15:
+            if self.grip_width < init_grip_width + self.safety_bound:
                 self.grip_width = self.grip_width + 1
                 self.send_gripper_command(None, self.grip_width)
-                rospy.sleep(0.7)
+                rospy.sleep(0.2)
+            else: 
+                rospy.logerr("Reached safety bound!")
+                return None
 
             # update contact bool
-            # for pillar in self.tac0_data.pillars:
-            #     if pillar.in_contact:
-            #         self.tac0_contact += 1
-            # for pillar in self.tac1_data.pillars:
-            #     if pillar.in_contact:
-            #         self.tac1_contact += 1
             total_force = self.tac0_data.gfZ + self.tac1_data.gfZ
-            # print('tac0_ contact', self.tac0_contact)
-            # print('tac1_contact', self.tac1_contact)
-            # print('force', 0.7 * total_force + init_ft_force)
-            if self.tac0_data.is_contact and self.tac1_data.is_contact and 0.4 * total_force + init_ft_force > fg:
+            print(total_force)
+            if self.tac0_data.is_contact and self.tac1_data.is_contact and self.fric_coef * total_force > init_ft_force:
                 both_contact = 1
-            # else:
-            #     self.tac0_contact, self.tac1_contact = 0, 0
 
         print("Grasped object.")
-        return True
+        return init_grip_width
 
     def save_data(self):
         with open("./src/tactile_data/scripts/fts_data.csv", "wb") as f:

src/slip_manipulation/state_estimator.py
@@ -17,17 +17,16 @@ class StateEstimator():
         
         # ros publishers and subscribers
         self.angle_pub = rospy.Publisher('/slip_manipulation/rotation_angle', AngleStamped, queue_size=1)
+        self.lowest_vertex_pub = rospy.Publisher('/slip_manipulation/lowest_vertex', Point, queue_size=1)
 
         # tf things
-        self.tf_broadcaster = tf2_ros.TransformBroadcaster()
-        
         self.tf_buffer = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
 
         # initialise
         self.upright_axis = self.get_upright_axis()
         
-    def estimate_contact_config(self, contact=True):
+    def vision_estimate_contact_config(self, contact=True):
 
         trans = patient_lookup_box_tf(self.tf_buffer, loop=True)
 
@@ -104,6 +103,36 @@ class StateEstimator():
         else:
             return True
 
+    def vision_estimate_lowest_vertex(self):
+        # init all eight vertices of the box
+        coords = []
+        coords.append((self.box_dim[0]/2, self.box_dim[1]/2, self.box_dim[2]/2))
+        coords.append((self.box_dim[0]/2, self.box_dim[1]/2, -self.box_dim[2]/2))
+        coords.append((self.box_dim[0]/2, -self.box_dim[1]/2, self.box_dim[2]/2))
+        coords.append((self.box_dim[0]/2, -self.box_dim[1]/2, -self.box_dim[2]/2))
+        coords.append((-self.box_dim[0]/2, self.box_dim[1]/2, self.box_dim[2]/2))
+        coords.append((-self.box_dim[0]/2, self.box_dim[1]/2, -self.box_dim[2]/2))
+        coords.append((-self.box_dim[0]/2, -self.box_dim[1]/2, self.box_dim[2]/2))
+        coords.append((-self.box_dim[0]/2, -self.box_dim[1]/2, -self.box_dim[2]/2))
+
+        z_height = []
+        for coord in coords:
+            # convert to base_link frame to get height from base
+            vertex = Pose(Point(*coord), Quaternion(0, 0, 0, 1))
+            base_vertex = tf_transform_pose(self.tf_buffer, vertex, 'box_origin', 'base_link', loop=False)
+            if base_vertex is None:
+                rospy.logerr('No box transform')
+                return
+            # save height in z dimension
+            z_height.append(base_vertex.pose.position.z)
+        
+        # find min
+        min_z_idx = np.argmin(z_height)
+        lowest_vertex = base_vertex[min_z_idx].pose.position
+        
+        # publish vertex as Point msg
+        self.lowest_vertex_pub.publish(lowest_vertex)
+
     def get_upright_axis(self):
         # check upright edge using transform from base_link
         trans = patient_lookup_box_tf(self.tf_buffer, loop=True)
@@ -149,7 +178,7 @@ class StateEstimator():
             print("Uncaught case for object position. No side facing up found.")
             return
 
-    def estimate_rotation_angle(self):
+    def vision_estimate_rotation_angle(self):
         try:
             trans = self.tf_buffer.lookup_transform('box_origin', 'base_link', rospy.Time(0), timeout=rospy.Duration(2))
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):

src/slip_manipulation/ur5_moveit.py
@@ -7,12 +7,8 @@ import roslib; roslib.load_manifest('robotiq_2f_gripper_control')
 from geometry_msgs.msg import PoseStamped
 from moveit_msgs.msg import Constraints, JointConstraint
 from slip_manipulation.get_tf_helper import *
-
-import tf2_ros
-import tf2_geometry_msgs
-
-import copy
-import math
+from moveit_msgs.srv import GetCartesianPath, GetCartesianPathRequest
+from moveit_msgs.msg import MoveItErrorCodes
 
 class UR5Moveit():
     
@@ -35,19 +31,23 @@ class UR5Moveit():
         # initialise joint constraints to prevent collision with camera mount
         self.init_moveit_constraints()
 
+        # set up service proxy for cartesian path planning
+        rospy.wait_for_service('/compute_cartesian_path', timeout=rospy.Duration(10))
+        self.cartesian_srv = rospy.ServiceProxy('/compute_cartesian_path', GetCartesianPath)
+
         # define important poses
         # self.arm.set_named_target("up") # go to up position if not already there
         self.start_pose = {
-            'shoulder_pan_joint': 0,
-            'shoulder_lift_joint': -np.pi/2,
-            'elbow_joint': -np.pi/2,
-            'wrist_1_joint': -np.pi/2,
+            'shoulder_pan_joint': -np.pi/2,
+            'shoulder_lift_joint': (-94) * np.pi/180,
+            'elbow_joint': (-65) * np.pi/180,
+            'wrist_1_joint': (-111) * np.pi/180,
             'wrist_2_joint': np.pi/2,
-            'wrist_3_joint': np.pi
+            'wrist_3_joint': np.pi/2
         }
 
         # move to start pose
-        # self.move_to_joints_pose(self.start_pose)
+        self.move_to_joints_goal(self.start_pose)
 
     def init_planning_scene(self):
         # add table collision object
@@ -63,19 +63,19 @@ class UR5Moveit():
         # clear existing constraints
         self.arm.clear_path_constraints()
         
-        self.camera_constraints = Constraints()
-        self.camera_constraints.name = 'camera'
+        # self.camera_constraints = Constraints()
+        # self.camera_constraints.name = 'camera'
         
-        joint_constraint = JointConstraint()
-        joint_constraint.joint_name = 'wrist_2_joint'
-        joint_constraint.position = 0
-        joint_constraint.tolerance_above = 2.55
-        joint_constraint.tolerance_below = 2.55
-        joint_constraint.weight = 1
+        # joint_constraint = JointConstraint()
+        # joint_constraint.joint_name = 'wrist_2_joint'
+        # joint_constraint.position = 0
+        # joint_constraint.tolerance_above = 2.55
+        # joint_constraint.tolerance_below = 2.55
+        # joint_constraint.weight = 1
         
-        self.camera_constraints.joint_constraints.append(joint_constraint)
+        # self.camera_constraints.joint_constraints.append(joint_constraint)
 
-        self.arm.set_path_constraints(self.camera_constraints)
+        # self.arm.set_path_constraints(self.camera_constraints)
         
 
     def move_to_joints_goal(self, goal_pose):
@@ -103,10 +103,38 @@ class UR5Moveit():
         
         raw_input('Check Rviz for cartesian plan, press enter to execute')
 
-        self.arm.execute(plan)
+        self.arm.execute(plan, wait=True)
 
     def display_pose(self, pose):
         self.pose_publisher.publish(pose)
+        
+    def compute_cartesian_path_with_start_state(self, start_state, waypoints, max_step, jump_threshold):
+        '''
+        :param start_state: starting state to plan from
+        :type start_state: moveit_msgs.msg.RobotoState
+        :param waypoints: a sequence of waypoints to plan through
+        :type waypoints: a list of geometry_msgs.msg.Pose
+        '''
+        req = GetCartesianPathRequest()
+        req.header.frame_id = 'base_link'
+        req.header.stamp = rospy.Time.now()
+        req.start_state = start_state
+        req.group_name = 'manipulator'
+        req.link_name = self.arm.get_end_effector_link()
+        req.waypoints = waypoints
+        req.max_step = max_step
+        req.jump_threshold = jump_threshold
+        req.avoid_collisions = True
+        
+        try:
+            res = self.cartesian_srv.call(req)
+        except rospy.ServiceException as exc:
+            print("Service did not process request: " + str(exc))
+            
+        if res.error_code.val == MoveItErrorCodes.SUCCESS:
+            return (res.solution, res.fraction)
+        else:
+            return (None, -1.0)
 
 if __name__ == "__main__":
     pass