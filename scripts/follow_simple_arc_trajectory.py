5f2367855a78f7374d981469aed7eab6920eea54
ericx1107 <xushiyu1107@gmail.com>
2024-10-23 21:07:34 +1100

    Add scripts to follow simple trajectory

---
+++ scripts/follow_simple_arc_trajectory.py
@@ -0,0 +1,163 @@
+#! /usr/bin/env python
+
+import rospy
+import numpy as np
+import copy
+import subprocess
+import time
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from geometry_msgs.msg import PoseStamped, WrenchStamped
+from moveit_msgs.msg import Constraints, OrientationConstraint
+from slip_manipulation.ur5_moveit import UR5Moveit
+from slip_manipulation.box_markers import BoxMarkers
+from slip_manipulation.sensorised_gripper import SensorisedGripper
+from slip_manipulation.arc_trajectory import ArcTrajectory
+from slip_manipulation.simple_arc_trajectory import SimpleArc
+from robotiq_ft_sensor.srv import sensor_accessor
+from papillarray_ros_v2.srv import BiasRequest
+
+class FollowSimpleTrajectory():
+    def __init__(self):
+        self.ur5 = UR5Moveit()
+
+        self.gripper = SensorisedGripper()
+        self.arc = SimpleArc(self.ur5, self.gripper)
+        
+        self.grasp_pub = rospy.Publisher('pregrasp_pose', PoseStamped, queue_size=1)
+        
+        self.ft_sub = rospy.Subscriber('/robotiq_ft_wrench', WrenchStamped, self.ft_callback)
+        self.ft_record = False
+        self.Fy_array = []
+        self.Fz_array = []
+        self.t_array = []
+
+        self.pregrasp_offset = 0.05
+        
+        self.ee_offset = 0.18
+
+        self.grasp_goal = PoseStamped()
+        
+        rospy.wait_for_service('/robotiq_ft_sensor_acc', timeout=rospy.Duration(10))
+        robotiq_sensor_srv = rospy.ServiceProxy('/robotiq_ft_sensor_acc', sensor_accessor)
+        try:
+            resp1 = robotiq_sensor_srv(command_id = 8)
+        except rospy.ServiceException as exc:
+            print("Service did not process request: " + str(exc))
+          
+        # tactile sensor bias #######################################
+        # rospy.wait_for_service('/hub_0/send_bias_request', timeout=rospy.Duration(10))
+        # tactile_sensor_srv = rospy.ServiceProxy('/hub_0/send_bias_request', BiasRequest)
+        # try:
+        #     resp2 = tactile_sensor_srv()
+        #     print('response:' , resp2)
+        #     # print('zeroed tactile sensor')
+        # except rospy.ServiceException as exc:
+        #     print("Service did not process request: " + str(exc))        
+        # tactile sensor bias #######################################
+
+    def ft_callback(self, data):
+        self.Fz = data.wrench.force.z
+        self.Fy = data.wrench.force.y
+        self.Tx = data.wrench.torque.x
+        
+        if self.ft_record:
+            self.Fz_array.append(data.wrench.force.z)
+            self.Fy_array.append(data.wrench.force.y)
+            self.t_array.append(data.header.stamp.to_nsec())
+
+    def plot_ft(self):
+        fig, ax = plt.subplots()
+    
+        ax.scatter(self.t_array, self.Fz_array, s=(mpl.rcParams['lines.markersize'] ** 2)/8)
+        ax.set_xlabel('Time (Unix epoch)')
+        ax.set_ylabel('Force in the z direction (N)')
+        
+        fig.savefig('/home/acrv/trajectory_ws/data/ft_' + str(time.time()) + '.png')
+
+if __name__ == "__main__":
+    rospy.init_node('simple_arc')
+    
+    while True:
+        
+        # initialise class object
+        demo = FollowSimpleTrajectory()
+        # wait for some topics to publish
+        # rospy.sleep(10)
+        
+        # get grasp goal while blocking
+        # demo.grasp_goal = rospy.wait_for_message('/slip_manipulation/grasp_pose', PoseStamped)  # get this in some other way #########
+        # demo.grasp_goal.pose.position.z += demo.ee_offset
+        demo.grasp_goal = demo.ur5.arm.get_current_pose()
+        
+        # move to goal
+        pregrasp_pose = copy.deepcopy(demo.grasp_goal)
+        pregrasp_pose.pose.position.z += demo.pregrasp_offset
+        
+        print("planning")
+        waypoints, start_pose = demo.arc.plan_arc_trajectory(radius=0.75)
+        
+        plan, _ = demo.ur5.arm.compute_cartesian_path([start_pose], # waypoints to follow
+										        0.01,       # eef_step  
+										        0.0)        # jump_threshold  
+        
+        raw_input('Check Rviz for cartesian plan, press enter to execute')
+        print("Start timing")
+        start_time = time.time()
+        
+        demo.ur5.arm.execute(plan, wait=True)
+        
+        # set gripper orientation constraint
+        gripper_constraints = Constraints()
+        gripper_constraints.name = 'gripper_constraint'
+            
+        ori_constraint = OrientationConstraint()
+        ori_constraint.link_name = 'wrist_3_link'
+        ori_constraint.orientation = demo.ur5.arm.get_current_pose().pose.orientation
+        ori_constraint.absolute_x_axis_tolerance = 5*np.pi/180
+        ori_constraint.absolute_y_axis_tolerance = 5*np.pi/180
+        ori_constraint.absolute_z_axis_tolerance = 5*np.pi/180
+        ori_constraint.weight = 1
+        
+        gripper_constraints.orientation_constraints.append(ori_constraint)
+
+        demo.ur5.arm.set_path_constraints(gripper_constraints)
+        
+        # demo.ur5.move_to_cartesian_goal(demo.grasp_goal.pose)
+
+        # close gripper
+        # raw_input("press enter to close gripper")
+        # demo.gripper.send_gripper_command(commandName='close')
+        # 138 smallest box feb10(fri)
+        # 103 square-ish box feb10(fri)
+        # 107 long box feb13(mon)
+
+
+        demo.arc.original = demo.ur5.arm.get_current_pose()
+        demo.arc.grasped = True
+        
+        (plan, _) = demo.ur5.arm.compute_cartesian_path(waypoints, 0.01, 0.0)
+
+        raw_input("check rviz before execute")
+        # demo.ft_record = True
+        demo.ur5.arm.execute(plan, wait=True)
+        
+        # demo.ft_record = False
+        print('total work ', (demo.arc.translational_work + demo.arc.rotational_work))
+        print('transalational work ', demo.arc.translational_work)
+        print('rotational work ',  demo.arc.rotational_work)
+        print('time', time.time()-start_time)
+
+        demo.plot_ft()
+        
+        # open gripper
+        # raw_input("press enter to open gripper")
+        # demo.gripper.send_gripper_command(commandName=None, grip_width=0)
+        
+        # move up
+        # end_goal = demo.ur5.arm.get_current_pose()
+        # end_goal.pose.position.z += 0.03
+        # demo.ur5.move_to_cartesian_goal(end_goal.pose)
+        
+        del demo
+        
---
+++ src/slip_manipulation/simple_arc_trajectory.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+# use moveit_commander (the Python MoveIt user interfaces )
+import copy
+import rospy
+import math
+import numpy as np
+import tf2_ros
+import time
+import moveit_commander
+from geometry_msgs.msg import Pose, Point, Quaternion
+from slip_manipulation.get_tf_helper import *
+from std_msgs.msg import Bool,Float32
+from geometry_msgs.msg import WrenchStamped
+from slip_manipulation.msg import AngleStamped
+from papillarray_ros_v2.msg import SensorState
+from slip_manipulation.sensorised_gripper import SensorisedGripper
+from control_msgs.msg import FollowJointTrajectoryActionGoal
+from actionlib_msgs.msg import GoalStatusArray
+from moveit_msgs.msg import RobotState
+
+class SimpleArc():
+    # pose: Current Pose of the end effector
+    # base_dim: box length that lays down on the ground
+    # height_dim: box height value
+    # long: boolean for if we are making the long direction of the box lay down on the ground
+    # arm: "the move_group_commander", used for cartesian path
+    # shift: the shifted value along the x direction
+    
+    '''
+    Always pivots towards the centre of box
+    '''
+    def __init__(self, ur5_moveit, gripper):
+        
+        self.listener = tf.TransformListener()
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+
+        self.ft_sub = rospy.Subscriber('/robotiq_ft_wrench', WrenchStamped, self.ft_callback)
+        self.tac_sub = rospy.Subscriber('/hub_0/sensor_0', SensorState, self.tac_callback)
+        self.gripper = gripper
+        
+        self.goal_pub = rospy.Publisher('/scaled_pos_joint_traj_controller/follow_joint_trajectory/goal', FollowJointTrajectoryActionGoal, queue_size=1)
+        
+        self.work_pub = rospy.Publisher('/slip_manipulation/work', Float32, queue_size=1)
+        
+        self.ur5 = ur5_moveit
+        self.arm = ur5_moveit.arm
+        self.robot = moveit_commander.RobotCommander()
+
+        self.theta = 0
+        self.tac_data = 0
+        # self.Fz = 0
+        # self.fg = 9.8 * box_weight * 1.5  # scaling shenanigans
+        # self.box_angle = np.arctan(self.height_dim/self.base_dim)
+
+        self.angle_err = 1e6 # set large number to initialise while loop
+        self.threshold = 5e-1
+        # PID constants
+        self.Kp = 2e-4#1e-4
+        self.Kd = 5e-4
+        self.Ki = 8e-5#3e-6
+        self.Kp_vision = 5e-1#1e-4
+        self.Ki_vision = 1e-2#3e-6
+        
+        self.z_offset = 0
+        self.x_offset = 0
+        
+        self.temp_loop = np.arange(5)
+        
+        self.dt = 0
+        self.prev_err = 0
+        self.intg_err = 0
+        
+        self.slip_type = 0
+        self.slip_type_x = 0
+        self.pillars_in_contact = 0
+        self.safety_bound = 40
+        
+        self.prev_dt = time.time()
+        
+        self.original = None
+        
+        self.prev_angle = 0
+        
+        # self.start_z = 0
+        # self.start_y = 0
+        # self.start_theta = 0
+        
+        # self.end_z = 0
+        # self.end_y = 0
+        # self.end_theta = 0
+        
+        self.translational_work = 0
+        self.rotational_work = 0
+        self.grasped = False
+        
+        
+        self.init_grip_width = None
+
+    def plan_arc_trajectory(self, radius):
+        '''plan an arc trajectory along the circumference of a circle with the given radius
+        Start with poses in the robot's end effector tool0 frame, then transform to base_link frame
+        '''
+        
+        waypoints = []
+        
+        # consider the centre of the circle to be on the table surface next to the base origin (y=0, z=0)
+        start_x = -0.40  # magic number on the table
+        start_y = 0.0  # magic number on the table
+        # use hard coded start and end angles
+        start_angle = np.pi / 4
+        end_angle = np.pi - start_angle
+        # calculate the starting position given the radius of the curve
+        # H = radius, theta = start_angle, O = z, A = y
+        start_pose = Pose(Point(0,0,0), Quaternion(0,0,0,1))
+        start_pose.orientation = self.arm.get_current_pose().pose.orientation
+        start_pose.position.z = radius * np.sin(start_angle)
+        start_pose.position.y = start_y + radius * np.cos(start_angle)
+        start_pose.position.x = start_x
+        waypoint_pose = copy.deepcopy(start_pose)
+        
+        for theta in np.linspace(start_angle, end_angle, 50):
+            waypoint_pose.position.z = radius * np.sin(theta)
+            waypoint_pose.position.y = start_y + radius * np.cos(theta)
+            
+            # wpose_base = tf_transform_pose(self.listener, waypoint_pose, 'tool0', 'base_link').pose
+            waypoints.append(copy.deepcopy(waypoint_pose))
+
+        return waypoints, start_pose
+        
+        # old
+        # Always assuming we start from the long direction
+        # z is up and down, x is the direction of "home", y is the side  
+
+        # waypoints = []
+        # wpose = Pose(Point(0,0,0), Quaternion(0,0,0,1))
+        
+        # r = np.hypot(self.base_dim, self.height_dim)
+        # box_angle = math.degrees(np.arctan(self.height_dim/self.base_dim))  + curr_angle
+        
+        #     # change is always positive
+        #     # cos is always positive -> change * cos is positive -> x is positive
+        #     # sin is always negative -> change * sin is negative -> y is negative
+        #     # int(np.ceil(abs(goal_angle - curr_angle)))
+        # for theta in np.linspace(box_angle, box_angle + goal_angle, 50):
+        #     wpose.position.z = -(r*math.sin(math.radians(theta))) + self.height_dim # - 0.03
+        #     wpose.position.y = self.base_dim - r*math.cos(math.radians(theta))
+            
+        #     wpose_base = tf_transform_pose(self.listener, wpose, 'tool0', 'base_link').pose
+        #     waypoints.append(copy.deepcopy(wpose_base))
+     
+        # return waypoints
+        
+    def ft_callback(self, data):
+        self.Fz = data.wrench.force.z
+        self.Fy = data.wrench.force.y
+        self.Tx = data.wrench.torque.x
+        
+        trans = patient_lookup_tf(self.tf_buffer, 'tool0')
+        
+        if self.grasped:
+            # print("Fz is ", self.Fz)
+            # print("Distance change is ", abs(trans.transform.translation.z - self.original.pose.position.z))
+            self.translational_work  += abs(self.Fz * abs(trans.transform.translation.z - self.original.pose.position.z))
+            self.translational_work  += abs(self.Fy * abs(trans.transform.translation.y - self.original.pose.position.y))
+            
+            self.original.pose.position.z = trans.transform.translation.z 
+            self.original.pose.position.y = trans.transform.translation.y
+        
+        
+            angle_diff = abs(self.theta - self.prev_angle) 
+            self.prev_angle = self.theta
+            
+            self.rotational_work += abs((angle_diff) * self.Tx)
+        
+        
+        
+        
+    def tac_callback(self, data):
+        self.tac_data = data
+        
+        if self.init_grip_width is not None:
+            self.slip_control(self.init_grip_width)
+        
+    # def force_pred(self):
+    #     # force = 20/np.pi * np.arctan(-8 * (self.theta - np.pi/5)) - 6
+    #     theta = np.pi/2 - self.box_angle - self.theta
+    #     alpha = np.pi/2 - theta
+    #     force = self.fg * np.sin(theta) * np.cos(alpha)/2
+    #     return force
+
+
+if __name__ == "__main__":
+    pass
